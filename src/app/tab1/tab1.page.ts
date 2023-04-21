8705e785f7c37b2b34ad93816d0920a3f21ab315
Andreas Diehl <adiehl@bitquadrat.de>
2023-04-21 14:22:18 +0200

    working

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,6 +3,7 @@ import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 import { CommonModule } from '@angular/common';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -10,11 +11,12 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['tab1.page.scss'],
   standalone: true,
   imports: [IonicModule, ExploreContainerComponent, CommonModule],
+  providers: [SocialSharing]
 })
 export class Tab1Page {
   public picture: string | null = null;
 
-  constructor() {}
+  constructor(private socialSharing: SocialSharing) {}
 
   async takePicture() {
     const image = await Camera.getPhoto({
@@ -25,5 +27,80 @@ export class Tab1Page {
     });
 
     this.picture = 'data:image/jpeg;base64,' + image.base64String;
+    await this.watermark();
   }
+
+  async watermark() {
+    const image = new Image();
+    image.src = this.picture || '';
+    image.onload = () => {
+      const canvas = document.createElement('canvas');
+      const context = canvas.getContext('2d');
+
+      canvas.width = image.width;
+
+      canvas.height = image.height;
+      if (context) {
+        console.log('Context');
+        // Draw the original image on the canvas
+        context.drawImage(image, 0, 0, image.width, image.height);
+
+        // Load the watermark image and draw it on the canvas
+        const watermark = new Image();
+        watermark.src = 'assets/watermark.png';
+        watermark.onload = () => {
+        const maxWidth = canvas.width * 0.3;
+        const maxHeight = canvas.height * 0.3;
+        const scale = Math.min(maxWidth / watermark.width, maxHeight / watermark.height);
+        const x = canvas.width - watermark.width * scale - canvas.width * 0.05;
+        const y = canvas.height - watermark.height * scale - canvas.height * 0.05;
+        context.drawImage(watermark, x, y, watermark.width * scale, watermark.height * scale);
+
+        this.picture = canvas.toDataURL('image/jpeg');
+          
+        };
+      } else {
+      }
+    };
+  }
+
+  share() {
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+    const image = new Image();
+
+    image.onload = () => {
+      canvas.width = image.width;
+      canvas.height = image.height;
+      if (context) {
+        // Draw the original image on the canvas
+        context.drawImage(image, 0, 0, image.width, image.height);
+
+        // Load the watermark image and draw it on the canvas
+        const watermark = new Image();
+        watermark.src = 'assets/watermark.png';
+        watermark.onload = () => {
+          const scale = 0.1;
+          const x = canvas.width - watermark.width * scale;
+          const y = canvas.height - watermark.height * scale;
+          context.drawImage(watermark, x, y, watermark.width * scale, watermark.height * scale);
+
+          // Share the image with the watermark on Instagram
+          this.socialSharing.shareViaInstagram(
+            'I shared 10 cent', // Caption
+            canvas.toDataURL('image/png'), // Image with watermark
+          );
+        };
+      }
+    };
+    if (this.picture) {
+      image.src = this.picture;
+    }
+    
+  }
+
+  clear() {
+    this.picture = null;
+  }
+
 }
