15824e0bb70fd7e7b75a1f123353a9eef74654bd
Andreas Diehl <adiehl@bitquadrat.de>
2023-04-21 16:29:15 +0200

    capacitor android

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.bitquadrat.waterisright',
+  appName: 'WaterIsRight',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
